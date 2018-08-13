5ca09bc90488b40e1d28e91344522c0106284e12
sam-pop <poplovitch.samuel@gmail.com>
2018-08-13 19:36:21 -0400

    refactoring

--- models/user.js
+++ models/user.js
@@ -19,7 +19,10 @@ const userSchema = new Schema({
       "Please fill a valid email address"
     ]
   },
-  password: { type: String, required: true }
+  password: { 
+    type: String, 
+    required: true 
+  }
 });
 
 const User = mongoose.model("User", userSchema);
