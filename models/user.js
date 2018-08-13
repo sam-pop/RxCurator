15803f157752a4965be44bbe732c2deeffb1dff5
sam-pop <poplovitch.samuel@gmail.com>
2018-08-13 19:31:21 -0400

    user model email validation added

--- models/user.js
+++ models/user.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const validateEmail = function(email) {
+  let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
 const userSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address"
+    ]
+  },
+  password: { type: String, required: true }
 });
 
 const User = mongoose.model("User", userSchema);
