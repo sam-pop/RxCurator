a917a7f9a3ac34046f01b6a337e4cb72734ab7bf
sam-pop <poplovitch.samuel@gmail.com>
2018-08-13 20:40:53 -0400

    refactoring

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const validateEmail = function(email) {
+const validateEmail = function(email){
   let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return reg.test(email);
 };
