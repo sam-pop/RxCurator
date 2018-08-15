9cd584c6a2ffdca7418b29895e28d2a20e0b8fbf
sam-pop <poplovitch.samuel@gmail.com>
2018-08-15 19:28:41 -0400

    yarn build

---
+++ client/build/assets/js/main.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  alert("hey!");
+  if (
+    navigator.mediaDevices &&
+    typeof navigator.mediaDevices.getUserMedia === "function"
+  ) {
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#test")
+        },
+        decoder: {
+          readers: ["ean_reader"]
+          // readers: ["code_128_reader"]
+        }
+      },
+      function(err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("Initialization finished. Ready to start");
+        Quagga.start();
+        Quagga.onDetected(function(data) {
+          console.log(data);
+          alert(data.codeResult.code);
+        });
+      }
+    );
+  }
+};
