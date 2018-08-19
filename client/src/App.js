7a9fb124f1dd61ac7032c6c5682f1f0e63982768
sam-pop <poplovitch.samuel@gmail.com>
2018-08-19 15:04:01 -0400

    react router integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,22 @@
-import React, { Component } from "react";
-import Scanner from "./components/Scanner";
-import Barcode from "./components/Barcode";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Scan from "./pages/Scan";
+// import Home from "./pages/Detail";
+// import Signin from "./pages/NoMatch";
+// import Nav from "./components/Nav";
 
-class App extends Component {
-  state = {
-    scanning: false,
-    barcodes: [],
-    meds: []
-  };
-
-  _scan = () => {
-    this.setState({ scanning: !this.state.scanning });
-  };
-
-  _onDetected = ean => {
-    console.log(this.state.barcodes);
-    if (this.state.barcodes.indexOf(ean) === -1)
-      this.setState({
-        barcodes: this.state.barcodes.concat([ean])
-      });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this._scan}>
-          {this.state.scanning ? "Stop" : "Start"}
-        </button>
-        {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null}
-        <ul className="barcodes">
-          {this.state.barcodes.map((barcode, i) => (
-            <Barcode key={barcode + i} barcode={barcode} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      {/* <Nav /> */}
+      <Switch>
+        <Route exact path="/" component={Scan} />
+        {/* <Route exact path="/books" component={Books} /> */}
+        {/* <Route exact path="/books/:id" component={Detail} /> */}
+        {/* <Route component={NoMatch} /> */}
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
---
+++ client/src/pages/Scan/Scan.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Scanner from "../../components/Scanner";
+import Barcode from "../../components/Barcode";
+
+class Scan extends Component {
+  state = {
+    scanning: false,
+    barcodes: [],
+    meds: []
+  };
+
+  _scan = () => {
+    this.setState({ scanning: !this.state.scanning });
+  };
+
+  _onDetected = ean => {
+    console.log(this.state.barcodes);
+    if (this.state.barcodes.indexOf(ean) === -1)
+      this.setState({
+        barcodes: this.state.barcodes.concat([ean])
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this._scan}>
+          {this.state.scanning ? "Stop" : "Start"}
+        </button>
+        {this.state.scanning ? <Scanner onDetected={this._onDetected} /> : null}
+        <ul className="barcodes">
+          {this.state.barcodes.map((barcode, i) => (
+            <Barcode key={barcode + i} barcode={barcode} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Scan;
