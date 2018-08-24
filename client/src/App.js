cfee2d62409368863168457643cd5d3063b05549
sam-pop <poplovitch.samuel@gmail.com>
2018-08-24 13:25:05 -0400

    homepage update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 import Scan from "./pages/Scan";
-import Signin from "./pages/Signin";
-// import Home from "./pages/Detail";
-// import Nav from "./components/Nav";
 
 const App = () => (
   <Router>
     <div>
       {/* <Nav /> */}
       <Switch>
+        <Route exact path="/" component={Home} />
         <Route exact path="/scan" component={Scan} />
       </Switch>
     </div>
